512b9a87a267004b66e7714e2704036722dfb347
niraiarin <niraiarin@gmail.com>
2021-06-20 12:40:14 +0000

    change zprezto -> zinit

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+## set .vimrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+## install zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+source ~/.zshrc
+zinit self-update
+cat ./zshrc_add >> ~/.zshrc
+
+
