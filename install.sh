7dbf20df70aa3e0446a345f99f4df18d350e0481
niraiarin <niraiarin@gmail.com>
2021-06-20 13:55:39 +0000

    reset zshrc

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 ## install zinit
-wget https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh -O ./install_zinit.sh && \
+apt-get install -y expect && \
+	: > ~/.zshrc && \
+	wget https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh -O ./install_zinit.sh && \
 	./zinit.exp && \
 	cat ./zshrc_add >> ~/.zshrc && \
 	rm ./install_zinit.sh
