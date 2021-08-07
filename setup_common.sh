237288c0fb87b46664e9020a0acf18187054eabd
niraiarin <niraiarin@gmail.com>
2021-08-07 12:20:52 +0900

    fix seperate common script and user script

--- setup_common.sh
+++ setup_common.sh
@@ -27,23 +27,6 @@ useradd -m -G wheel -s "$shell" "$username" &&\
 echo "${username} ALL=(ALL) ALL" >> /etc/sudoers
 
 ## change user
-su "$username" << EOF
-pwd
-
-## install dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
-rm installer.sh
-
-## set .vimrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-
-## install zinit
-: > ~/.zshrc && \
-	wget https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh -O ./install_zinit.sh && \
-	./zinit.exp && \
-	cat ./zshrc_add >> ~/.zshrc && \
-	rm ./install_zinit.sh
-
+su "${username}" << EOF
+sh ./setup_user.sh
 EOF
